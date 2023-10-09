50ec9a37300f2ed098750deaeff7394d8e7fd86e
Noel John <noelcj9@gmail.com>
2023-10-09 16:59:57 +0530

    Update task1.py
    (Update) - Updating few elements

--- src/task1.py
+++ src/task1.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # coding: utf-8
 """
-    :author: pk13055
+    :author: noelcj9
     :brief:  This script is used to perform the task 1 of the assignment 1.
 """
 import pandas as pd
+import numpy as np
+import math 
 
 
 def main():
